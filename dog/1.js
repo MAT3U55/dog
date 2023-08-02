1fa329c6bfef40f2705b4fe489aeec96894f067f
MAT3U55 <123771977+MAT3U55@users.noreply.github.com>
2023-08-02 13:22:51 -0300

    Add files via upload

---
+++ dog/1.js
@@ -0,0 +1,15 @@
+let MyAge = 16
+/*Armazena a sua idade em anos humanos*/
+let earlyYears = 2;
+/*Os dois primeiros anos de um cão.*/
+earlyYears *= 10.5;
+/*Multiplica earlyYears por 10.5 para obter "anos de cão*/
+laterYears *= 4;
+/*Multiplica laterYears por 4 para obter "anos de cão"*/
+console.log(earlyYears, laterYears);
+
+let myAgeInDogYears = earlyYears + laterYears;
+/* Calcula a sua idade em "anos de cão".*/
+let myName = "Mateus".toLowerCase()
+/* Converte seu nome para letras minúsculas.*/
+console.log(`Meu nome é ${myName}. Tenho ${myAge} anos em anos humanos, que são ${myAgeInDogYears} anos em anos de cão.`);
